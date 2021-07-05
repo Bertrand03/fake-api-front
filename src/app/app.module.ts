dd6a2da7627bec7a622b7286d765ba4a427576b3
bertrand.cerot <bertrand.cerot@cgi.com>
2021-07-05 11:19:02 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddPostComponent } from './views/add-post/add-post.component';
+import { EditPostComponent } from './views/edit-post/edit-post.component';
+import { SinglePostComponent } from './views/single-post/single-post.component';
+import { PostsComponent } from './views/posts/posts.component';
+import { PostFormComponent } from './components/post-form/post-form.component';
+import { ErrorsFormComponent } from './components/errors-form/errors-form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddPostComponent,
+    EditPostComponent,
+    SinglePostComponent,
+    PostsComponent,
+    PostFormComponent,
+    ErrorsFormComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
